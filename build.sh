217cd763212da145b87a2f557296b26c0d963132
linastorvaldz <creamylofly@gmail.com>
2025-12-14 10:14:51 +0800

    [TEMP] disable manual hooking for  KernelSU-Next
    Signed-off-by: linastorvaldz <creamylofly@gmail.com>

--- build.sh
+++ build.sh
@@ -212,7 +212,7 @@ else
 fi
 
 # KSU Manual Hooks
-if ksu_included && ! susfs_included && ! [ "$KSU" == "Biasa" ]; then
+if ksu_included && ! susfs_included && ! [ "$KSU" == "Biasa" ] || ! [ "$KSU" == "Next" ]; then
   log "Applying manual hook patch"
   if [ "$KSU" == "Suki" ]; then
     patch -p1 --forward < $KERNEL_PATCHES/hooks/manual-hook-v1.6.patch
