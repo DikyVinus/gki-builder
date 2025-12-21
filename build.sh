b8a4bdfb9211a86ed92490514bb97bd3a2462859
BoltX-Official <151031855+Kingfinik98@users.noreply.github.com>
2025-12-21 12:32:30 +0700

    build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ fi
 KERNEL_NAME="BoltX"
 USER="boltx"
 HOST="boltx"
-TIMEZONE="Asia/makasar"
+TIMEZONE="Asia/Makassar"
 ANYKERNEL_REPO="https://github.com/linastorvaldz/anykernel"
 KERNEL_DEFCONFIG="quartix_defconfig"
 if [ "$KVER" == "6.6" ]; then
