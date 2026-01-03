7a3ffd54ebca1a7e06c182641fae7d69e37463e3
RZN <151031855+Kingfinik98@users.noreply.github.com>
2026-01-04 00:04:23 +0700

    Create gki_defconfig.sh

---
+++ inject_ksu/gki_defconfig.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Definisikan lokasi defconfig target
+DEFCONFIG="arch/arm64/configs/gki_defconfig"
+
+echo "⚙️ Menambahkan konfigurasi KSU-Next & SuSFS"
+
+# Base KSU Config & Dependencies
+cat >> $DEFCONFIG <<EOF
+# ===============================================
+# Konfigurasi KernelSU Next (Base)
+CONFIG_KSU=y
+# Kprobes is a hard dependency for KSU-Next
+CONFIG_KPROBES=y
+CONFIG_KPROBE_EVENTS=y
+EOF
+
+# Logika pemilihan metode hook berdasarkan env KSU_SUSFS
+if [ "$KSU_SUSFS" = "true" ]; then
+  echo "🔧 Mode: SuSFS Hook Enabled"
+  cat >> $DEFCONFIG <<EOF
+# --- SuSFS Configuration ---
+CONFIG_KSU_SUSFS=y
+CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y
+CONFIG_KSU_SUSFS_SUS_PATH=y
+CONFIG_KSU_SUSFS_SUS_MOUNT=y
+CONFIG_KSU_SUSFS_SUS_KSTAT_SPOOF_GENERIC=y
+CONFIG_KSU_SUSFS_SUS_KSTAT=y
+CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y
+CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y
+CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSTAT=y
+CONFIG_KSU_SUSFS_SUS_OVERLAYFS=n
+CONFIG_KSU_SUSFS_TRY_UMOUNT=y
+CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y
+CONFIG_KSU_SUSFS_SPOOF_UNAME=y
+CONFIG_KSU_SUSFS_ENABLE_LOG=y
+CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y
+CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y
+CONFIG_KSU_SUSFS_OPEN_REDIRECT=y
+# Disable other hooks
+CONFIG_KSU_KPROBES_HOOK=n
+CONFIG_KSU_MANUAL_HOOK=n
+CONFIG_KSU_HAS_MANUAL_HOOK=n
+CONFIG_KSU_SUSFS_SUS_SU=n
+CONFIG_KSU_SYSCALL_HOOK=n
+EOF
+
+else
+  echo "🔧 Mode: Kprobes Hook Standard"
+  cat >> $DEFCONFIG <<EOF
+# --- Kprobes Hook Method ---
+# Disable SuSFS and Manual Hook
+CONFIG_KSU_SUSFS=n
+CONFIG_KSU_SUSFS_SUS_SU=n
+CONFIG_KSU_MANUAL_HOOK=n
+CONFIG_KSU_HAS_MANUAL_HOOK=n
+CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=n
+CONFIG_KSU_SYSCALL_HOOK=n
+EOF
+fi
