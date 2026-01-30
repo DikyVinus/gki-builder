8105c4d4428a963b15ece248f9e215a2936e4534
BoltX <151031855+Kingfinik98@users.noreply.github.com>
2026-01-30 21:06:56 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ elif [ "$KVER" == "5.10" ]; then
 elif [ "$KVER" == "6.1" ]; then
   RELEASE="v0.1"
 fi
-KERNEL_NAME="Enfiled-Kernel"
+KERNEL_NAME="Enfiled-Quix"
 USER="king"
 HOST="BoltX"
 TIMEZONE="Asia/Jakarta"
