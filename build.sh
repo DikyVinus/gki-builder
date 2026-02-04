531909194ddad1e03aeeab3ec96b795f0a61a41c
BoltX <151031855+Kingfinik98@users.noreply.github.com>
2026-02-04 19:30:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -34,9 +34,9 @@ elif [ "$KVER" == "6.1" ]; then
   ANYKERNEL_BRANCH="android14-6.1"
   KERNEL_BRANCH="android14-6.1-staging"
 elif [ "$KVER" == "5.10" ]; then
-  KERNEL_REPO="https://github.com/linastorvaldz/kernel-android12-5.10"
+  KERNEL_REPO="https://github.com/ramabondanp/android_kernel_common-5.10.git"
   ANYKERNEL_BRANCH="android12-5.10"
-  KERNEL_BRANCH="master"
+  KERNEL_BRANCH="android12-5.10-staging"
 fi
 DEFCONFIG_TO_MERGE=""
 GKI_RELEASES_REPO="https://github.com/Kingfinik98/BoltX-Release"
