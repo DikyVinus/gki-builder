d1c3ac1a4d76cf9e5a77100d1d06d59bac5d2ebb
DikyVinus <137989414+DikyVinus@users.noreply.github.com>
2026-02-20 05:16:10 +0700

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 
-log() { echo "[LOG] $*"; }
-error() { echo "ERROR: $*" && exit 1; }
-upload_file() { :; }
-send_msg() { :; }
-simplify_gh_url() { echo "$1"; }
-ksu_included() { [ "$KSU" = "yes" ]; }
-susfs_included() { [ "$KSU_SUSFS" = "true" ]; }
+log() {
+  echo "[LOG] $*"
+}
+
+error() {
+  echo "[ERROR] $*"
+  return 0
+}
+
+_error() {
+  echo "[ERROR] $*"
+  return 0
+}
+
+upload_file() {
+  return 0
+}
+
+send_msg() {
+  return 0
+}
+
+simplify_gh_url() {
+  echo "$1"
+}
+
+ksu_included() {
+  [ "$KSU" = "yes" ]
+}
+
+susfs_included() {
+  [ "$KSU_SUSFS" = "true" ]
+}
